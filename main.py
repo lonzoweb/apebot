700d85f3da8f0c371b01a6cb490b9e68fdd50abb
LonzoWeb <lonzosly@gmail.com>
2025-11-10 12:24:35 -0800

    log level

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ import tasks
 # ============================================================
 
 logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
